88a30e7132aeffb8a23569542044573931191311
baozhoutao <baozhoutao@hotoa.com>
2023-09-08 16:54:41 +0800

    Update package.service.js

--- steedos-packages/cms/package.service.js
+++ steedos-packages/cms/package.service.js
@@ -2,8 +2,8 @@
  * @Author: baozhoutao@steedos.com
  * @Date: 2023-08-04 09:12:17
  * @LastEditors: baozhoutao@steedos.com
- * @LastEditTime: 2023-08-07 14:00:36
+ * @LastEditTime: 2023-09-08 16:54:24
  * @Description: 
  */
-const service = require("./src/index.js");
+const service = require("./dist/index.js");
 module.exports = service;
